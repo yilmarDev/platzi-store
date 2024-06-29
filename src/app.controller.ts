40f6215f1d3b744fea9d09e67b3cb1a60bbf28e5
YilmarDev <yilmardev@gmail.com>
2024-06-28 21:03:42 -0500

    Receiving params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,40 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    return 'Hola Mundo de Node';
+  }
+
+  @Get(`endpoint1`)
+  endpoint1() {
+    return `I'm new 1`;
+  }
+
+  @Get(`endpoint2`)
+  endpoint2() {
+    return `I'm new 2`;
+  }
+
+  @Get(`products/:id`)
+  getProducts(@Param() params: any) {
+    return `Product number ${params.id}`;
+  }
+
+  @Get(`product-categories/:id`)
+  getProductsCategories(@Param('id') id: string) {
+    return `Product category number ${id}`;
+  }
+
+  @Get(`products/:productId/categories/:categoryId`)
+  getProductsByCategory(
+    @Param('productId') productId: string,
+    @Param('categoryId') categoryId: string,
+  ) {
+    return `Product ${productId} filter by category number ${categoryId}`;
+  }
+
+  @Get(`clients/:id`)
+  getClients(@Param('id') id: string) {
+    return `Client number ${id}`;
   }
 }
