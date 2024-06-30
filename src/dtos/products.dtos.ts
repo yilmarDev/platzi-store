3b9212095371fac12a0a6f3b4a423f4e4bfaaf60
YilmarDev <yilmardev@gmail.com>
2024-06-30 18:30:49 -0500

    Class 20 - Validando clases parciales

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,34 @@
+import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   readonly price: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
